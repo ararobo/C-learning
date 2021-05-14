855aa2d8b79d5c3fda80181c19b083e74acc775e
Yudai-desktop <yudai.yy0804@gmail.com>
2021-05-14 10:30:36 +0900

    1st commit

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void){
+    int nensyu = 100;   //万円
+    float gessyu = 8.33;    //万円
+
+    printf("僕の年収は%d万円なので、\n",nensyu);
+    printf("月収は%f万円になります。\n",gessyu);
+    
+    return 0;
+}
---
+++ ex2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 200;
+    int b = 100;
+    int c;
+    c = a + b;
+    printf("a + b = %d\n",c);
+    c = a - b;
+    printf("a - b = %d\n",c);
+    c = a * b;
+    printf("a * b = %d\n",c);
+    c = a / b;
+    printf("a / b = %d\n",c);
+    return 0;
+}
+
